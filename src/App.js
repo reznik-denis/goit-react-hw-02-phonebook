93a65594a70690c238e11d1a65de14035339ba5e
Reznik Denis <63472391+reznik-denis@users.noreply.github.com>
2020-12-09 22:19:42 +0200

    add all code

--- src/App.js
+++ src/App.js
@@ -1,44 +1,60 @@
 import './App.css';
 import { Component } from 'react';
+import ContactForm from './Components/ContactForm';
+import ContactList from './Components/ContactList';
+import Filter from './Components/Filter';
+import shortid from 'shortid';
+
+
+const contacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
 class App extends Component {
   state = {
-  contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ],
+  contacts: contacts,
   filter: '',
-  name: '',
-  number: ''
   }
-  
+
+  formSubmitHendler = ({name, number}) => {
+    const contact = {
+      id: shortid.generate(),
+      name,
+      number,
+    };
+    if (this.state.contacts.find(cont => cont.name === contact.name)) {
+      alert(`${contact.name} is already in contacts`);
+      return
+    }
+    this.setState(({contacts}) => ({
+      contacts: [contact, ...contacts]
+    }));
+  };
+
+  deleteContact = idContact => {
+    this.setState(({contacts}) => ({
+      contacts: contacts.filter(contact => contact.id !== idContact)
+    }));
+  }
+
+  chengeFilter = event => {
+    this.setState({filter: event.currentTarget.value});
+  }
+
   render() {
-    const contacts = this.state.contacts;
-      return (
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    const visibleContacts = contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter));
+    return (
     <div className="App">
       <h1>Phonebook</h1>
-      <form className="formBlock">
-        <label className="labelBlock">
-          Name <input type="text" className="inputStyles"/>
-        </label>
-        <label className="labelBlock">
-          Number <input type="text" className="inputStyles"/>
-        </label>
-        <button type="submit" className="button">Add contact</button>
-      </form>
-          <h2>Contacts</h2>
-          <label className="labelBlock">
-          Find contacts by name <input type="text" className="inputStyles"/>
-        </label>
-          <ul className="list">
-            {contacts.map((contact) => (
-              <li key={contact.id} className="listItem">{contact.name}: {contact.number}
-              <button type="button" className="buttonDelete">Delete</button>
-              </li>
-              ))}
-          </ul>
+          <ContactForm onSubmit={this.formSubmitHendler}/>
+      <h2>Contacts</h2>
+        <Filter value={filter} onChange={this.chengeFilter}/>
+        <ContactList contacts={visibleContacts} onDeleteContact={this.deleteContact}/>
     </div>
   );
   }}
