85f9af2a34036d2406219d442dc856e1ae0a17ff
Reznik Denis <63472391+reznik-denis@users.noreply.github.com>
2020-12-09 18:46:42 +0200

    add html and css in app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
+class App extends Component {
+  state = {
+  contacts: [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+  ],
+  filter: '',
+  name: '',
+  number: ''
+  }
+  
+  render() {
+    const contacts = this.state.contacts;
+      return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Phonebook</h1>
+      <form className="formBlock">
+        <label className="labelBlock">
+          Name <input type="text" className="inputStyles"/>
+        </label>
+        <label className="labelBlock">
+          Number <input type="text" className="inputStyles"/>
+        </label>
+        <button type="submit" className="button">Add contact</button>
+      </form>
+          <h2>Contacts</h2>
+          <label className="labelBlock">
+          Find contacts by name <input type="text" className="inputStyles"/>
+        </label>
+          <ul className="list">
+            {contacts.map((contact) => (
+              <li key={contact.id} className="listItem">{contact.name}: {contact.number}
+              <button type="button" className="buttonDelete">Delete</button>
+              </li>
+              ))}
+          </ul>
     </div>
   );
-}
+  }}
 
 export default App;
